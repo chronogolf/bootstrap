83b431eaf35a4f6daaa5a7bce911e14cebd2fec8
V Bergeron <veronique.bergeron@lightspeedhq.com>
2026-02-09 16:18:20 -0500

    feat(datepicker): add aria labels and aria level

--- README.md
+++ README.md
@@ -3,8 +3,6 @@ Due to [Angular](https://angular.io)'s continued adoption, our creation of [the 
 
 We thank you for all your contributions over the years and hope you've enjoyed using this library as much as we've had developing and maintaining it.  It would not have been successful without them.
 
-THIS IS A TEST PUSH TO CHRONOGOLF FORK
-
 ---
 
 ### UI Bootstrap - [AngularJS](http://angularjs.org/) directives specific to [Bootstrap](http://getbootstrap.com)
--- src/datepicker/datepicker.js
+++ src/datepicker/datepicker.js
@@ -18,6 +18,8 @@ angular.module('ui.bootstrap.datepicker', ['ui.bootstrap.dateparser', 'ui.bootst
   minMode: 'day',
   monthColumns: 3,
   ngModelOptions: {},
+  nextButtonLabel: 'next',
+  previousButtonLabel: 'previous',
   shortcutPropagation: false,
   showWeeks: true,
   yearColumns: 5,
@@ -56,6 +58,8 @@ angular.module('ui.bootstrap.datepicker', ['ui.bootstrap.dateparser', 'ui.bootst
     'minDate',
     'minMode',
     'monthColumns',
+    'nextButtonLabel',
+    'previousButtonLabel',
     'showWeeks',
     'shortcutPropagation',
     'startingDay',
@@ -87,6 +91,11 @@ angular.module('ui.bootstrap.datepicker', ['ui.bootstrap.dateparser', 'ui.bootst
       case 'yearColumns':
       case 'yearRows':
         self[key] = angular.isDefined($scope.datepickerOptions[key]) ?
+          $scope.datepickerOptions[key] : datepickerConfig[key];
+        break;
+      case 'nextButtonLabel':
+      case 'previousButtonLabel':
+        $scope[key] = angular.isDefined($scope.datepickerOptions[key]) ?
           $scope.datepickerOptions[key] : datepickerConfig[key];
         break;
       case 'startingDay':
